66f73241332e9157ba647074990e8e255e97ee1b
ottley1 <77740349+ottley1@users.noreply.github.com>
2021-01-20 19:49:53 +0000

    Images moved out of root into folders.
    Both product images and QR codes are now located inside of their own folders, product images henceforth will be stored directly and source links to images have been updated to reflect this.

--- productLookup.js
+++ productLookup.js
@@ -2,11 +2,11 @@ function check(){
   if(document.getElementById("product").value == "Dobbies Multi-Purpose John Innes Reduced Peat Content, 60L"){
     document.getElementById("selectedItemName").innerHTML = "Dobbies Multi-Purpose John Innes Reduced Peat Content, 60L";
     document.getElementById("navCode").innerHTML = "Item Number: 601684";
-    document.getElementById("selectedItemImage").src = "https://hub.dobbies.com/ImageGen.ashx?image=/media/13567713/601684-Dobbies-Multi-Purpose-John-Innes-Reduced-Peat-Content-60L.jpg&Class=ProductDetailImage";
+    document.getElementById("selectedItemImage").src = "productImages/601684-Dobbies-Multi-Purpose-John-Innes-Reduced-Peat-Content-60L.jpeg";
     document.getElementById("selectedItemImage").style.height = "350px";
     document.getElementById("selectedItemImage").style.width = "350px";
 
-    document.getElementById("selectedItemImageQR").src = "dobMultiCompRP.png";
+    document.getElementById("selectedItemImageQR").src = "productQR/dobMultiCompRP.png";
     document.getElementById("selectedItemImageQR").style.height = "350px";
     document.getElementById("selectedItemImageQR").style.width = "350px";
   }
